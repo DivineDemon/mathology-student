6a4e2ffe0a6d2e78384a6b432bab1ea3faab705e
ahmad-saqi <ahmad_saqi@digimarkdevelopers.com>
2025-03-20 10:35:12 +0500

    search

--- src/pages/problems.tsx
+++ src/pages/problems.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
-import { ChevronLeft, ChevronRight, Loader2, Search } from "lucide-react";
+import { ChevronDown, ChevronLeft, ChevronRight, Loader2, Search } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 import Sort from "@/assets/img/sort.svg";
@@ -161,15 +161,83 @@ const Problems = () => {
           <SidebarTrigger className="block lg:hidden" />
           <div className="text-3xl font-bold lg:text-4xl">Questions</div>
         </div>
-        <div className="flex items-center justify-center rounded-lg border border-primary/20 bg-white pl-2.5">
-          <Search className="text-primary" />
-          <Input
-            type="text"
-            placeholder="Search"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="border-none bg-transparent shadow-none focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
-          />
+        <div className="flex gap-2">
+          <div className="flex items-center justify-center rounded-lg border border-primary/20 bg-white pl-2.5">
+            <Search className="text-primary" />
+            <Input
+              type="text"
+              placeholder="Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="border-none bg-transparent shadow-none focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0"
+            />
+
+          </div>
+          <div className="flex gap-2">
+            <DropdownMenu >
+              <DropdownMenuTrigger asChild>
+                <Button
+                  type="button"
+                  className="w-40 flex justify-between"
+                  variant="outline"
+                >
+                 <span> All Standard </span><ChevronDown/>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-24">
+                <DropdownMenuItem
+                  onClick={() => setSelectedStandard(null)}
+                >
+                  All
+                </DropdownMenuItem>
+                {standards?.map((standard) => (
+                  <DropdownMenuItem
+                    key={standard.standard_id}
+                    onClick={() =>
+                      setSelectedStandard(standard.standard_title)
+                    }
+                  >
+                    {standard.standard_title}
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  className="w-40"
+                  type="button"
+                  variant="outline"
+                >
+                 <span> Difficulty Level</span> <ChevronDown/>
+                 
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem
+                  onClick={() => setSelectedDifficulty(null)}
+                >
+                  All
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => setSelectedDifficulty("easy")}
+                >
+                  Easy
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => setSelectedDifficulty("medium")}
+                >
+                  Medium
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => setSelectedDifficulty("hard")}
+                >
+                  Hard
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </nav>
       {isLoading ? (
@@ -177,187 +245,126 @@ const Problems = () => {
           <Loader2 className="size-10 animate-spin text-primary" />
         </div>
       ) : //@ts-ignore
-      currentData?.length > 0 ? (
-        <div className="mx-auto flex h-full w-full flex-col justify-between gap-5 p-5">
-          <div className="w-full">
-            <Table>
-              <TableHeader className="truncate">
-                <TableRow>
-                  <TableHead>Question Title</TableHead>
-                  <TableHead>Course</TableHead>
-                  <TableHead>Lesson</TableHead>
-                  <TableHead>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          type="button"
-                          className="w-full"
-                          variant="ghost"
-                        >
-                          Standard&nbsp;
-                          <img src={Sort} alt="sort-icon" className="size-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent className="w-24">
-                        <DropdownMenuItem
-                          onClick={() => setSelectedStandard(null)}
-                        >
-                          All
-                        </DropdownMenuItem>
-                        {standards?.map((standard) => (
-                          <DropdownMenuItem
-                            key={standard.standard_id}
-                            onClick={() =>
-                              setSelectedStandard(standard.standard_title)
-                            }
-                          >
-                            {standard.standard_title}
-                          </DropdownMenuItem>
-                        ))}
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </TableHead>
-                  <TableHead>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button
-                          className="w-full"
-                          type="button"
-                          variant="ghost"
-                        >
-                          Difficulty Level&nbsp;
-                          <img src={Sort} alt="sort-icon" className="size-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent>
-                        <DropdownMenuItem
-                          onClick={() => setSelectedDifficulty(null)}
-                        >
-                          All
-                        </DropdownMenuItem>
-                        <DropdownMenuItem
-                          onClick={() => setSelectedDifficulty("easy")}
-                        >
-                          Easy
-                        </DropdownMenuItem>
-                        <DropdownMenuItem
-                          onClick={() => setSelectedDifficulty("medium")}
-                        >
-                          Medium
-                        </DropdownMenuItem>
-                        <DropdownMenuItem
-                          onClick={() => setSelectedDifficulty("hard")}
-                        >
-                          Hard
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </TableHead>
-                  <TableHead>Tags</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody className={cn("text-md", "truncate text-ellipsis")}>
-                {currentData?.map((question, index) => (
-                  <TableRow
-                    key={index}
-                    className={cn("cursor-pointer bg-gray-100", {
-                      "bg-white": index % 2 === 0,
-                    })}
-                    onClick={() =>
-                      navigate(`/question-attempt/${question.question_id}`)
-                    }
-                    title={question.question_title}
-                  >
-                    <TableCell className="overflow-hidden truncate font-medium">
-                      {truncateString(question.question_title, 10)}
-                    </TableCell>
-                    <TableCell className="overflow-hidden truncate">
-                      {truncateString(question.course_title, 10)}
-                    </TableCell>
-                    <TableCell className="overflow-hidden truncate">
-                      {truncateString(question.lesson_title, 10)}
-                    </TableCell>
-                    <TableCell className="text-center">
-                      {question.standard_title}
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex w-full items-center justify-center">
-                        <div
-                          className={cn(
-                            "w-fit text-center font-semibold capitalize",
-                            {
-                              "rounded-lg bg-green-200 px-2 py-0.5 text-green-700":
-                                question.difficulty_level === "easy",
-                              "rounded-lg bg-[#FEEBC8] px-2 py-0.5 text-yellow-700":
-                                question.difficulty_level === "medium",
-                              "rounded-lg bg-red-200 px-2 py-0.5 text-red-700":
-                                question.difficulty_level === "hard",
-                            }
-                          )}
-                        >
-                          {question.difficulty_level}
-                        </div>
-                      </div>
-                    </TableCell>
-                    <TableCell
-                      className={cn(
-                        "flex w-full gap-1.5 overflow-hidden truncate p-2"
-                      )}
-                    >
-                      {question.skill_tags.slice(0, 2).map((tag, tagIndex) => (
-                        <span
-                          key={tagIndex}
-                          className={cn(
-                            "w-fit shrink-0 rounded-md bg-white p-2 text-center font-medium",
-                            { "bg-muted": index % 2 === 0 }
-                          )}
-                        >
-                          {truncateString(tag, 5)}
-                        </span>
-                      ))}
-                    </TableCell>
+        currentData?.length > 0 ? (
+          <div className="mx-auto flex h-full w-full flex-col justify-between gap-5 p-5">
+            <div className="w-full">
+              <Table>
+                <TableHeader className="truncate">
+                  <TableRow>
+                    <TableHead>Question Title</TableHead>
+                    <TableHead>Course</TableHead>
+                    <TableHead>Lesson</TableHead>
+                    <TableHead>Standard
+
+                    </TableHead>
+                    <TableHead>Difficulty Level
+
+                    </TableHead>
+                    <TableHead>Tags</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-          <div className="flex w-full items-center justify-between p-2">
-            <span className="text-sm">
-              Showing&nbsp;
-              <span className="font-semibold">
-                {startItem}-{endItem}
+                </TableHeader>
+                <TableBody className={cn("text-md", "truncate text-ellipsis")}>
+                  {currentData?.map((question, index) => (
+                    <TableRow
+                      key={index}
+                      className={cn("cursor-pointer bg-gray-100", {
+                        "bg-white": index % 2 === 0,
+                      })}
+                      onClick={() =>
+                        navigate(`/question-attempt/${question.question_id}`)
+                      }
+                      title={question.question_title}
+                    >
+                      <TableCell className="overflow-hidden truncate font-medium">
+                        {truncateString(question.question_title, 10)}
+                      </TableCell>
+                      <TableCell className="overflow-hidden truncate">
+                        {truncateString(question.course_title, 10)}
+                      </TableCell>
+                      <TableCell className="overflow-hidden truncate">
+                        {truncateString(question.lesson_title, 10)}
+                      </TableCell>
+                      <TableCell className="text-center">
+                        {question.standard_title}
+                      </TableCell>
+                      <TableCell>
+                        <div className="flex w-full items-center justify-center">
+                          <div
+                            className={cn(
+                              "w-fit text-center font-semibold capitalize",
+                              {
+                                "rounded-lg bg-green-200 px-2 py-0.5 text-green-700":
+                                  question.difficulty_level === "easy",
+                                "rounded-lg bg-[#FEEBC8] px-2 py-0.5 text-yellow-700":
+                                  question.difficulty_level === "medium",
+                                "rounded-lg bg-red-200 px-2 py-0.5 text-red-700":
+                                  question.difficulty_level === "hard",
+                              }
+                            )}
+                          >
+                            {question.difficulty_level}
+                          </div>
+                        </div>
+                      </TableCell>
+                      <TableCell
+                        className={cn(
+                          "flex w-full gap-1.5 overflow-hidden truncate p-2"
+                        )}
+                      >
+                        {question.skill_tags.slice(0, 2).map((tag, tagIndex) => (
+                          <span
+                            key={tagIndex}
+                            className={cn(
+                              "w-fit shrink-0 rounded-md bg-white p-2 text-center font-medium",
+                              { "bg-muted": index % 2 === 0 }
+                            )}
+                          >
+                            {truncateString(tag, 5)}
+                          </span>
+                        ))}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+            <div className="flex w-full items-center justify-between p-2">
+              <span className="text-sm">
+                Showing&nbsp;
+                <span className="font-semibold">
+                  {startItem}-{endItem}
+                </span>
+                &nbsp; from <span className="font-semibold">{totalItems}</span>
+                &nbsp; Questions
               </span>
-              &nbsp; from <span className="font-semibold">{totalItems}</span>
-              &nbsp; Questions
-            </span>
-            <div className="flex items-center gap-2">
-              <button
-                type="button"
-                onClick={() => handlePageChange(currentPage - 1)}
-                disabled={currentPage === 1}
-                className="rounded-2xl border-2 border-gray-300 bg-gray-100 p-2.5 text-primary hover:bg-primary hover:text-white"
-              >
-                <ChevronLeft className="size-6" />
-              </button>
-              <div className="w-fit rounded-2xl border-2 border-gray-300 bg-gray-100 text-primary">
-                {renderPagination()}
+              <div className="flex items-center gap-2">
+                <button
+                  type="button"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  disabled={currentPage === 1}
+                  className="rounded-2xl border-2 border-gray-300 bg-gray-100 p-2.5 text-primary hover:bg-primary hover:text-white"
+                >
+                  <ChevronLeft className="size-6" />
+                </button>
+                <div className="w-fit rounded-2xl border-2 border-gray-300 bg-gray-100 text-primary">
+                  {renderPagination()}
+                </div>
+                <button
+                  type="button"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                  className="rounded-2xl border-2 border-gray-300 bg-gray-100 p-2.5 text-primary hover:bg-primary hover:text-white"
+                >
+                  <ChevronRight className="size-6" />
+                </button>
               </div>
-              <button
-                type="button"
-                onClick={() => handlePageChange(currentPage + 1)}
-                disabled={currentPage === totalPages}
-                className="rounded-2xl border-2 border-gray-300 bg-gray-100 p-2.5 text-primary hover:bg-primary hover:text-white"
-              >
-                <ChevronRight className="size-6" />
-              </button>
             </div>
           </div>
-        </div>
-      ) : (
-        <div className="flex h-full w-full items-center justify-center">
-          <NotFound />
-        </div>
-      )}
+        ) : (
+          <div className="flex h-full w-full items-center justify-center">
+            <NotFound />
+          </div>
+        )}
     </div>
   );
 };
